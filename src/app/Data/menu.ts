003109ff60397e162e667be370c3f089a657cdbe
Leandro Soto <soto.lean@gmail.com>
2025-05-04 17:12:37 -0300

    se agregaron fotos de tortas

--- src/app/Data/menu.ts
+++ src/app/Data/menu.ts
@@ -4,6 +4,7 @@ export interface Producto {
   description: string;
   price: string;
   imageUrl: string;
+  hoverImageUrl: string;
   icon: string;
 }
 
@@ -14,7 +15,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "ascension",
       description: "Base húmeda de chocolate sin harina rellena de crema de dulce de leche.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/ascension.jpg",
+      imageUrl: "assets/img/Tortas/Torta Asensión/Torta-Asesion-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta Asensión/Torta-Asesion-Costado.webp",
       icon: ""
     },
     {
@@ -22,7 +24,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "new-york-cheesecake",
       description: "Base de galletitas, crema de queso cocido, compota de frutos rojos y frutos rojos frescos.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/cheesecake-ny.jpg",
+      imageUrl: "assets/img/Tortas/Torta New York CheeseCake/Torta-Cheesecake-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta New York CheeseCake/Torta-Cheesecake-Costado.webp",
       icon: ""
     },
     {
@@ -30,7 +33,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "chini-brownie",
       description: "Base de brownie, dulce de leche, crema y merengue.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/chini-brownie.jpg",
+      imageUrl: "assets/img/Tortas/Torta Brownie/Torta-Brownie-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta Brownie/Torta-Brownie-Costado.webp",
       icon: ""
     },
     {
@@ -38,7 +42,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "tiramisu",
       description: "Base de vainillas humedecidas con almíbar de café y una crema de queso mascarpone.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/tiramisu.jpg",
+      imageUrl: "assets/img/Tortas/Torta Tiramisú/Torta-Tiramisu-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta Tiramisú/Torta-Tiramisu-Costado.webp",
       icon: ""
     },
     {
@@ -46,7 +51,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "flor-del-alba",
       description: "Base de merengue de almendras y una crema de queso muy suave.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/flor-del-alba.jpg",
+      imageUrl: "assets/img/tortas/flor-del-alba.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -54,7 +60,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "oreo-cheesecake",
       description: "Base de galletitas oreo, dulce de leche y una crema con crocante de galletitas oreo.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/oreo-cheesecake.jpg",
+      imageUrl: "assets/img/Tortas/Torta oreo Cheese Cake/Torta-OreoCheesecake-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta oreo Cheese Cake/Torta-OreoCheesecake-Costado.webp",
       icon: ""
     },
     {
@@ -62,7 +69,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "dulce-otono",
       description: "Base de brownie, corazón de dulce de leche, mousse de chocolate, brillo de chocolate.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/dulce-otono.jpg",
+      imageUrl: "assets/img/Tortas/Torta dulce otoño/Torta-DulceOtoño-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta dulce otoño/Torta-DulceOtoño-Costado.webp",
       icon: ""
     },
     {
@@ -70,7 +78,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "tres-chocolates",
       description: "Base de vainilla, corazón de dulce de leche, mousse de chocolate con leche, mousse de chocolate semiamargo y mousse de chocolate blanco.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/tres-chocolates.jpg",
+      imageUrl: "assets/img/Tortas/Tres chocolates/Torta-TresChocolates-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Tres chocolates/Torta-TresChocolates-Costado.webp",
       icon: ""
     },
     {
@@ -78,7 +87,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "mousse-maracuya",
       description: "Base de vainilla, mousse de maracuyá, corazón de chocolate blanco.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/maracuya.jpg",
+      imageUrl: "assets/img/Tortas/Torta Mousse Maracuya/Torta-MousseMaracuya-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta Mousse Maracuya/Torta-MousseMaracuya-Costado.webp",
       icon: ""
     },
     {
@@ -86,7 +96,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "mousse-frutilla",
       description: "Base de bizcochuelo de vainilla, almíbar de frutilla y mousse de frutilla.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/frutilla.jpg",
+      imageUrl: "assets/img/tortas/frutilla.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -94,7 +105,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "matilda-argento",
       description: "Bizcochuelo de chocolate bien húmedo relleno de dulce de leche y chocolate, baño de chocolate.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/matilda.jpg",
+      imageUrl: "assets/img/Tortas/Matilda/Torta-Matilda-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Matilda/Torta-Matilda-Costado.webp",
       icon: ""
     },
     {
@@ -102,7 +114,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "honor-dona-petrona",
       description: "Bizcochuelo de vainilla humedecido con almíbar y un toque de coñac, crema chantilly con frutillas, dulce de leche y merenguitos.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/dona-petrona.jpg",
+      imageUrl: "assets/img/Tortas/Torta Doña Petrona/Torta-DoñaPetrona-Arriba.webp",
+      hoverImageUrl: "assets/img/Tortas/Torta Doña Petrona/Torta-DoñaPetrona-Costado.webp",
       icon: ""
     },
     {
@@ -110,17 +123,19 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "mil-hojas",
       description: "Capas de masa hojaldrada con relleno de dulce de leche.",
       price: "xxxx",
-      imageUrl: "assets/img/tortas/mil-hojas.jpg",
+      imageUrl: "assets/img/Tortas/Torta Rogel/Copia de Copia de Rogel.heif",
+      hoverImageUrl: "",
       icon: ""
     }
-  ],
+  ],  
   tartas: [
     {
       title: "Stroisel de Manzana",
       slug: "stroisel-manzana",
       description: "Base de tarta de vainilla, rellena con compota de manzana y streusel.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/stroisel-manzana.jpg",
+      imageUrl: "assets/img/tartas/stroisel-manzana.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -128,7 +143,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "tarta-pistacho",
       description: "Base de tarta de vainilla, crema de pistacho, frutillas frescas y arándanos.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/pistacho.jpg",
+      imageUrl: "assets/img/tartas/pistacho.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -136,7 +152,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "alice-orange-pie",
       description: "Base de tarta de vainilla, crema de naranja fresca y naranjas confitadas.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/orange-pie.jpg",
+      imageUrl: "assets/img/tartas/orange-pie.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -144,7 +161,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "tarta-pera-avellanas",
       description: "Base de tarta de chocolate, compota de peras y chocolate, streusel de avellanas.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/pera-avellanas.jpg",
+      imageUrl: "assets/img/tartas/pera-avellanas.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -152,7 +170,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "capuchina",
       description: "Base de tarta de chocolate, crema capuchina, crema chantilly y un toque de cacao y canela.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/capuchina.jpg",
+      imageUrl: "assets/img/tartas/capuchina.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -160,7 +179,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "lemon-pie",
       description: "Base de tarta de vainilla, rellena de crema de limón y merengue.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/lemon-pie.jpg",
+      imageUrl: "assets/img/tartas/lemon-pie.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -168,7 +188,8 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "tarta-frutilla",
       description: "Base de tarta de vainilla, crema chantilly, frutillas frescas y gelatina de frutilla. Opción: a pedido con crema pastelera.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/frutilla.jpg",
+      imageUrl: "assets/img/tartas/frutilla.webp",
+      hoverImageUrl: "",
       icon: ""
     },
     {
@@ -176,8 +197,9 @@ export const MENU_DATA: Record<string, Producto[]> = {
       slug: "tarta-choco-naranja",
       description: "Base de tarta de chocolate, mousse de chocolate, biscuit de vainilla y mousse de naranja.",
       price: "xxxx",
-      imageUrl: "assets/img/tartas/choco-naranja.jpg",
+      imageUrl: "assets/img/tartas/choco-naranja.webp",
+      hoverImageUrl: "",
       icon: ""
     }
-  ]
+  ]  
 };
